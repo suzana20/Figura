c9f32c5f9127f8f6b86404fbff4be546bc8cccaa
stivn <wilstivenortega@gmail.com>
2025-05-16 07:57:38 -0500

    merge local

--- src/LogicaPresentacion/Main.java
+++ src/LogicaPresentacion/Main.java
@@ -6,23 +6,6 @@ import LogicaNegocio.Figura;
 import LogicaNegocio.Rectangulo;
 
 public class Main {
-    //calcular el área y el perímetro
-    // de cualquier figura sin importar su tipo
-    public static void calcularOperaciones(Figura figura) {
-        System.out.println(figura);
-        if (figura instanceof Circulo) {
-
-            String perimetro = String.format("%.2f", figura.perimetro());
-            String area = String.format("%.2f", figura.area());
-
-            System.out.println("Perimetro: " + perimetro + " u"
-                                + "\nArea: " + area + " u");
-        } else {
-            System.out.println("Area: " + figura.area() + " u");
-            System.out.println("Perimetro: " + figura.perimetro() + " u");
-        }
-
-    }
 
     public static void main(String[] args) {
         Figura cuadrado = new Cuadrado(5);
